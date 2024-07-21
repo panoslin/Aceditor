3c7e154cb849b9fdeff4c6c2341049588bd7f287
Panos <panoslin@foxmail.com>
2024-07-21 16:40:49 +0800

    add: Common fonts in Quill Editor toolbar

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { EditorModule } from 'primeng/editor';
-import {FormsModule} from "@angular/forms";
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {EditorModule} from 'primeng/editor';
+import {FormsModule} from '@angular/forms';
+import Quill from 'quill';
 
 @Component({
   selector: 'app-editor',
@@ -9,7 +10,123 @@ import {FormsModule} from "@angular/forms";
   templateUrl: './editor.component.html',
   styleUrl: './editor.component.scss'
 })
-export class EditorComponent {
-  text: string | undefined;
+export class EditorComponent implements AfterViewInit {
+  @ViewChild('editor', {static: true}) editor!: ElementRef;
+  @ViewChild('toolbar', {static: true}) toolbar!: ElementRef;
+  quill!: Quill;
+//   text: string = `
+//                 <h1 class="ql-align-center">Quill Rich Text Editor</h1>
+//                 <p><br></p>
+//                 <p>Quill is a free, <a href="https://github.com/slab/quill/">open source</a> WYSIWYG editor built for the modern web. With its <a href="https://quilljs.com/docs/modules/">modular architecture</a> and expressive <a href="https://quilljs.com/docs/api">API</a>, it is completely customizable to fit any need.</p>
+//                 <p><br></p>
+//                 <iframe class="ql-video ql-align-center" src="https://player.vimeo.com/video/253905163" width="500" height="280" allowfullscreen></iframe>
+//                 <p><br></p>
+//                 <h2 class="ql-align-center">Getting Started is Easy</h2>
+//                 <p><br></p>
+//                 <pre data-language="javascript" class="ql-syntax" spellcheck="false"><span class="hljs-comment">// &lt;link href="https://cdn.jsdelivr.net/npm/quill@2.0.2/dist/quill.snow.css" rel="stylesheet"&gt;</span>
+// <span class="hljs-comment">// &lt;script src="https://cdn.jsdelivr.net/npm/quill@2.0.2/dist/quill.js"&gt;&lt;/script&gt;</span>
+//
+// <span class="hljs-keyword">const</span> quill = <span class="hljs-keyword">new</span> Quill(<span class="hljs-string">'#editor'</span>, {
+//   modules: {
+//     toolbar: <span class="hljs-string">'#toolbar'</span>
+//   },
+//   theme: <span class="hljs-string">'snow'</span>
+// });
+//
+// <span class="hljs-comment">// Open your browser's developer console to try out the API!</span>
+// </pre>
+//                 <p><br></p>
+//                 <p><br></p>
+//                 <p class="ql-align-center"><strong>Built with</strong></p>
+//                 <p class="ql-align-center"><span class="ql-formula" data-value="x^2 + (y - \\sqrt[3]{x^2})^2 = 1"></span></p>
+//                 <p><br></p>
+// `;
+  // toolbarOptions = [
+  //   // [{'bold': ['bold', 'italic', 'underline', 'strike']}],
+  //   [
+  //     'bold',
+  //     'italic',
+  //     'underline',
+  //     'strike'
+  //   ],        // toggled buttons
+  //   [
+  //     // 'blockquote',
+  //     'code-block'
+  //   ],
+  //   [
+  //     'link',
+  //     'image',
+  //     'video',
+  //     'formula'
+  //   ],
+  //   // [
+  //   //   { 'header': 1 },
+  //   //   { 'header': 2 }
+  //   // ],               // custom button values
+  //   [
+  //     {'list': 'ordered'},
+  //     {'list': 'bullet'},
+  //     {'list': 'check'}
+  //   ],
+  //   // [
+  //   //   { 'script': 'sub'},
+  //   //   { 'script': 'super' }
+  //   // ],      // superscript/subscript
+  //   // [
+  //   //   { 'indent': '-1'},
+  //   //   { 'indent': '+1' }
+  //   // ],          // outdent/indent
+  //   // [
+  //   //   { 'direction': 'rtl' }
+  //   // ],                         // text direction
+  //   // [
+  //   //   {
+  //   //     'size': ['small', false, 'large', 'huge']
+  //   //   }
+  //   // ],  // custom dropdown
+  //   [
+  //     {
+  //       'header':
+  //         [1, 2, 3, 4, 5, 6, false]
+  //     }
+  //   ],
+  //   [
+  //     {'color': []},
+  //     {
+  //       'background': []
+  //     }
+  //   ],          // dropdown with defaults from theme
+  //   [
+  //     {
+  //       'font': []
+  //     }
+  //   ],
+  //   [
+  //     {
+  //       'align': []
+  //     }
+  //   ],
+  //   [
+  //     'clean'
+  //   ]                                         // remove formatting button
+  // ];
+
+  ngAfterViewInit() {
+    this.quill = new Quill(this.editor.nativeElement, {
+      modules: {
+        // syntax: true,
+        toolbar: this.toolbar.nativeElement,
+      },
+      theme: 'snow',
+      // placeholder: this.text,
+    });
+
+    // Add fonts to whitelist
+    const Font = Quill.import('formats/font');
+    // @ts-ignore
+    Font.whitelist = ['arial', 'comic-sans', 'courier-new', 'georgia', 'helvetica', 'lucida', 'times-new-roman'];
+    // @ts-ignore
+    Quill.register(Font, true);
+  }
 
 }
