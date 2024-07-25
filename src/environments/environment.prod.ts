b9db796d02a30b181f53b445399b5a630e268a96
Panos <panoslin@foxmail.com>
2024-07-25 13:02:25 +0800

    add: chatGPT api config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    production: true
+  production: true,
+  chatGptApiEndpoint: 'https://api.openai.com/v1/chat/completions'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-    production: false
+  production: false,
+  chatGptApiEndpoint: 'https://api.openai.com/v1/chat/completions'
 };
 
 /*
